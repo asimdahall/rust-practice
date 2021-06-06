ddffd74eec4d0cc963a312d55a7d22a610714429
Asim Dahal <asim.dahal@joinsage.com>
2021-06-06 12:23:19 +0545

    practice: struct and enums together

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+enum IPAddressKind {
+    V4,
+    V6,
+}
+struct Connection {
+    ipAddressType: IPAddressKind,
+    address: String,
+}
+
+impl Connection {
+    fn print_ip_address_type(&self) {}
+}
 fn main() {
-    println!("Hello world");
+    let ipAddressInfo = Connection {
+        ipAddressType: IPAddressKind::V4,
+        address: String::from("192.168.1.1"),
+    };
 }
