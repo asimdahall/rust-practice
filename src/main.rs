d63f9cad14f7273573e3f331020f5ed794718f75
Asim Dahal <asim.dahal@joinsage.com>
2021-06-06 12:01:59 +0545

    learn: struct implementation

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,33 @@ struct Rect {
     width: u32,
     height: u32,
 }
+
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, second_rect: &Rect) -> bool {
+        if self.area() > second_rect.area() {
+            return true;
+        }
+        false
+    }
+}
 fn main() {
     let rect = Rect {
         width: 30,
         height: 30,
     };
+    let second_rect = Rect {
+        width: 20,
+        height: 30,
+    };
     println!("The rect is {:#?}", rect);
-    println!("The area of the rectangle is {} sq unit", area(&rect))
-}
-
-fn area(Rect { width, height }: &Rect) -> u32 {
-    width * height
+    println!("The area of the rectangle is {} sq unit", rect.area());
+    if rect.can_hold(&second_rect) {
+        println!("Yes the first rectangle can hold the second one")
+    } else {
+        println!("NO, SORRY :( the first rectangle cannot hold the second one");
+    }
 }
