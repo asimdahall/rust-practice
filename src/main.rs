b80e4adad369ac17cef6f34a149b9efadb1e42c1
Asim Dahal <asim.dahal@joinsage.com>
2021-06-07 07:24:37 +0545

    learn:match arms

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+enum people {
+    Ram,
+    Shyam,
+    Hari,
+    Kalu,
+}
+
 fn main() {
-    println!("Hello world");
+    greet_people(&people::Kalu)
+}
+
+fn greet_people(person: &people) {
+    match person {
+        people::Ram => {
+            println!("Hello I am Ram Dai, I am don")
+        }
+        people::Hari => {
+            println!("Hello I am Hari Dai, I am don")
+        }
+        people::Shyam => {
+            println!("Hello I am Shyam Dai, I am don")
+        }
+        _ => {
+            println!("Hello I am the default case")
+        }
+    }
 }
